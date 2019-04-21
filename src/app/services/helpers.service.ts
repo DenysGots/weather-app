9e3d38594a1e05117be88e18b1b7ecf90e280035
DenysGots <gotsdo@gmail.com>
2019-04-21 18:24:44 +0300

    SunCalc module import fixed for Heroku

--- src/app/services/helpers.service.ts
+++ src/app/services/helpers.service.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import * as moment from 'moment';
-import * as sunCalc from 'sunCalc';
+import * as sunCalc from 'suncalc';
 
 import { MoonPhases } from '../../../shared/public-api';
 
