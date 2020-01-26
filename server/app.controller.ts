a590d51518824568bb617426bae07f4989817e7b
DenysGots <gotsdo@gmail.com>
2020-01-26 23:06:07 +0200

    Heroku-testing

--- server/app.controller.ts
+++ server/app.controller.ts
@@ -1,7 +1,7 @@
 import { of } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 
-import { Controller, Ip, Post } from '@nestjs/common';
+import { Controller, Ip, Post, Headers, Req } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { LocationDto } from '../shared/public-api';
@@ -11,10 +11,18 @@ export class AppController {
   constructor(private appService: AppService) {}
 
   @Post('weather')
-  public async getWeather(@Ip() clientIp: any) {
-    // TODO: delete
-    console.log('Ip from request: ', clientIp);
-    this.appService.clearIpAddress(clientIp);
+  public async getWeather(@Headers() headers: any, @Req() request: any, @Ip() clientIp: any) {
+    // TODO: test, fix, delete
+    console.log('Request: ', request);
+    console.log('Headers from request: ', headers);
+
+    console.log('req.headers[x-forwarded-for]: ', req.headers && req.headers['x-forwarded-for']);
+    console.log('req.connection.remoteAddress: ', req.connection && req.connection.remoteAddress);
+    console.log('req.socket.remoteAddress: ', req.socket && req.socket.remoteAddress);
+    console.log('req.connection.socket.remoteAddress: ', req.connection && req.connection.socket && req.connection.socket.remoteAddress);
+
+    // console.log('Ip from request: ', clientIp);
+    // this.appService.clearIpAddress(clientIp);
     return of({});
 
     // return this.appService.getLocation(clientIp)
