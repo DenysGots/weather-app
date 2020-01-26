a68f6498191576007967f12ed6c51e57e9dc78f3
DenysGots <gotsdo@gmail.com>
2020-01-26 23:45:30 +0200

    Heroku-testing

--- server/app.controller.ts
+++ server/app.controller.ts
@@ -13,23 +13,29 @@ export class AppController {
   @Post('weather')
   public async getWeather(@Headers() headers: any, @Req() request: any, @Ip() clientIp: any) {
     // TODO: test, fix, delete
-    console.log('Request: ', request);
-    console.log('Headers from request: ', headers);
+    // console.log('Request: ', request);
+    // console.log('Headers from request: ', headers);
 
+    // This one works => 94.76.111.246
     console.log('req.headers[x-forwarded-for]: ', request.headers && request.headers['x-forwarded-for']);
-    console.log('req.connection.remoteAddress: ', request.connection && request.connection.remoteAddress);
-    console.log('req.socket.remoteAddress: ', request.socket && request.socket.remoteAddress);
-    console.log('req.connection.socket.remoteAddress: ', request.connection && request.connection.socket && request.connection.socket.remoteAddress);
+    // TODO: check this one
+    console.log('headers[x-forwarded-for]: ', headers && headers['x-forwarded-for']);
+
+    // This three doesn't => 10.11.72.189
+    // console.log('req.connection.remoteAddress: ', request.connection && request.connection.remoteAddress);
+    // console.log('req.socket.remoteAddress: ', request.socket && request.socket.remoteAddress);
+    // console.log('req.connection.socket.remoteAddress: ', request.connection && request.connection.socket && request.connection.socket.remoteAddress);
 
     // console.log('Ip from request: ', clientIp);
     // this.appService.clearIpAddress(clientIp);
-    return of({});
+    // return of({});
 
-    // return this.appService.getLocation(clientIp)
-    //   .pipe(
-    //     mergeMap((clientLocation: LocationDto) => this.appService.getWeather(clientLocation)),
-    //     map((weatherData: any) => this.appService.adjustReceivedData(weatherData))
-    //   );
+    const clientIp = request.headers['x-forwarded-for'];
+
+    return this.appService.getLocation(clientIp).pipe(
+      mergeMap((clientLocation: LocationDto) => this.appService.getWeather(clientLocation)),
+      map((weatherData: any) => this.appService.adjustReceivedData(weatherData))
+    );
   }
 
   // @Post('weather')
