a56c4ddb30c59a203f044cde5f1edbbe54ece47b
Denys Gots <Denys.Hots@solarwinds.com>
2020-02-20 11:02:06 +0200

    Code-cleanup

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,11 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 
-// (window as any).environment = environment;
-//
-// if (environment.production) {
-//   enableProdMode();
-// }
+(window as any).environment = environment;
+
+if (environment.production) {
+  enableProdMode();
+}
 
 export { renderModule, renderModuleFactory } from '@angular/platform-server';
 export { AppServerModule } from './app/app.server.module';
